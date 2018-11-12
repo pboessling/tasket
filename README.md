5287bf5987949e35873c57a6e3f5ce624b021675
Philippe Boessling <phib@phib.de>
2018-11-12 23:41:39 +0100

    Changed frontend from Thymeleaf to Vue.js

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Tasket
-
-A simple task management tool.
-
-Work in progress...
-
-## Requirements
-
-- JDK 8
-- Node.js
-- npm
-
-## Setup Local Development
-
-Install Vue.js:
-
-```
-npm install --global vue-cli
-```
+   
+   A simple task management tool.
+   
+   Work in progress...
+   
+   ## Requirements
+   
+   - JDK 8
+   - Node.js
+   - npm
+   
+   ## Setup Local Development
+   
+   Install Vue.js:
+   
+   ```
+   npm install --global vue-cli
+   ```
--- tasket-frontend/src/components/TaskBoard.vue
+++ tasket-frontend/src/components/TaskBoard.vue
@@ -7,37 +7,74 @@
 
     <h4>Backend response: {{ response }}</h4>
 
+    <h4>Taskboard:</h4>
+    {{ taskboard }}
+    <table>
+      <tr>
+        <th>To Do</th>
+        <th>Planned</th>
+        <th>Selected</th>
+        <th>Done</th>
+      </tr>
+      <tr>
+        <td>
+          <ul>
+            <li v-for="item in taskboard.data.tasksTodo">
+              <span>{{ item.title }}</span>
+              <a href="">Edit</a>
+              <a href="">Delete</a>
+            </li>
+          </ul>
+        </td>
+        <td>
+...
+        </td>
+        <td>
+...
+        </td>
+        <td>
+...
+        </td>
+      </tr>
+    </table>
+
   </div>
 </template>
 
 <script>
-  // import axios from 'axios'
-  import {AXIOS} from './http-common'
+// import axios from 'axios'
+import {AXIOS} from './http-common'
 
-  export default {
-    name: 'service',
-    data() {
-      return {
-        msg: 'HowTo call REST-Services:',
-        response: [],
-        errors: []
-      }
-    },
-    methods: {
-      // Fetches posts when the component is created.
-      callRestService() {
-        AXIOS.get(`/tasks`)
-          .then(response => {
-            // JSON responses are automatically parsed.
-            this.response = response.data
-            console.log(response.data)
-          })
-          .catch(e => {
-            this.errors.push(e)
-          })
-      }
+export default {
+  name: 'service',
+  data () {
+    return {
+      msg: 'HowTo call REST-Services:',
+      taskboard: null,
+      response: [],
+      errors: []
     }
+  },
+  methods: {
+    // Fetches posts when the component is created.
+    callRestService () {
+      AXIOS.get(`/tasks`)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.response = response.data
+          console.log(response.data)
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    }
+  },
+  mounted () {
+    AXIOS
+      .get('tasks')
+      .then(response => (this.taskboard = response))
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
