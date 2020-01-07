2728ff4f8c9ca332b6a602850888fb103dbbdd35
Philippe Boessling <phib@phib.de>
2020-01-07 23:51:29 +0100

    docs: add javadoc comments

--- src/main/java/de/phib/tasket/data/collection/Collection.java
+++ src/main/java/de/phib/tasket/data/collection/Collection.java
@@ -11,6 +11,9 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Entity representing a collection.
+ */
 @Entity
 public class Collection {
 
@@ -37,51 +40,110 @@ public class Collection {
     @JsonManagedReference
     private List<Note> notes;
 
+    /**
+     * Creates a new instance of Collection.
+     */
     public Collection() {
     }
 
+    /**
+     * Creates a new instance of Collection.
+     *
+     * @param id the id of the collection
+     */
     public Collection(String id) {
         this.id = id;
     }
 
+    /**
+     * Creates a new instance of Collection.
+     *
+     * @param localDate the date of the collection
+     */
     public Collection(LocalDate localDate) {
         this.localDate = localDate;
     }
 
+    /**
+     * Returns the id of the collection.
+     *
+     * @return the id of the collection.s
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Sets the id of the collection.
+     *
+     * @param id the id to set
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Returns the title of the collection.
+     *
+     * @return the title of the collection
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the collection
+     *
+     * @param title the title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Returns the date of the collection.
+     *
+     * @return the date of the collection
+     */
+    // TODO: Might be optional, depending on the type of Collection?
     public LocalDate getLocalDate() {
         return localDate;
     }
 
+    /**
+     * Sets the date of the collection.
+     *
+     * @param localDate the date of the collection
+     */
     public void setLocalDate(LocalDate localDate) {
         this.localDate = localDate;
     }
 
+    /**
+     * Returns the events of the collection.
+     *
+     * @return the events of the collection
+     */
     //TODO: Consider defensive copy
     public List<Event> getEvents() {
         return this.events;
     }
 
+    /**
+     * Returns the tasks of the collection.
+     *
+     * @return the tasks of the collection
+     */
     //TODO: Consider defensive copy
     public List<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Returns the notes of the collection.
+     *
+     * @return the notes of the collection
+     */
     //TODO: Consider defensive copy
     public List<Note> getNotes() {
         return notes;
--- src/main/java/de/phib/tasket/data/collection/CollectionController.java
+++ src/main/java/de/phib/tasket/data/collection/CollectionController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * API controller for creating, reading, updating, and deleting collections.
+ */
 // TODO: Is this Controller still needed?
 @RestController
 @RequestMapping("/api/collections")
@@ -14,6 +17,11 @@ public class CollectionController {
 
     private CollectionService collectionService;
 
+    /**
+     * Creates a new instance of CollectionController.
+     *
+     * @param collectionService a CollectionService
+     */
     @Autowired
     public CollectionController(CollectionService collectionService) {
         this.collectionService = collectionService;
@@ -21,21 +29,44 @@ public class CollectionController {
 
     // collections - start
 
+    /**
+     * Returns all collections.
+     *
+     * @return all collections
+     */
     @GetMapping("")
     public Iterable<Collection> getAllCollections() {
         return this.collectionService.getAllCollections();
     }
 
+    /**
+     * Creates a new collection in the repository.
+     *
+     * @param collection the collection to create
+     * @return the created collection
+     */
     @PostMapping("")
     public Collection createCollection(@RequestBody Collection collection) {
         return this.collectionService.createCollection(collection);
     }
 
+    /**
+     * Retrieves a collection from the repository by id.
+     *
+     * @param id the id of the collection
+     * @return the retrieved collection
+     */
     @GetMapping("/{id}")
     public Collection getCollection(@PathVariable("id") String id) {
         return this.collectionService.getCollection(id);
     }
 
+    /**
+     * Retrieves a collection from the repository by date.
+     *
+     * @param date the date of the collection
+     * @return the retrieved collection
+     */
     @GetMapping("/byDate/{date}")
     public Collection getCollectionByDate(@PathVariable("date") String date) {
         // TODO: Handle invalid date - DateTimeParseException - 400 bad request
@@ -44,11 +75,23 @@ public class CollectionController {
         return this.collectionService.getCollectionByLocalDate(LocalDate.parse(date));
     }
 
+    /**
+     * Updates a collection in the repository by id.
+     *
+     * @param id         the id of the collection
+     * @param collection the collection
+     * @returnthe updated collection
+     */
     @PostMapping("/{id}")
     public Collection updateCollection(@PathVariable("id") String id, @RequestBody Collection collection) {
         return this.collectionService.updateCollection(id, collection);
     }
 
+    /**
+     * Deletes a collection from the repository by id.
+     *
+     * @param id the id of the collection
+     */
     @DeleteMapping("/{id}")
     public void deleteCollection(@PathVariable("id") String id) {
         this.collectionService.deleteCollection(id);
@@ -58,6 +101,13 @@ public class CollectionController {
 
     // events - start
 
+    /**
+     * Retrieves the events of a collection from the repository.
+     *
+     * @param collectionId the id of the collection
+     * @return the events of the collection
+     */
+    // TODO: Is this still needed? If not, remove it. If yes, maybe implement analog methods for tasks and notes?
     @GetMapping("/{collectiondId}/events")
     public List<Event> getEvents(@PathVariable("collectionId") String collectionId) {
         //return this.collectionService.getCollection(collectionId).getEvents();
