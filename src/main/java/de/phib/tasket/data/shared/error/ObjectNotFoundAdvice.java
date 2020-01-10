eff8acfaaa5f7ced97e75ca48e6a58fb06e0930e
Philippe Boessling <phib@phib.de>
2020-01-10 22:20:25 +0100

    docs: add javadoc comments

--- src/main/java/de/phib/tasket/data/shared/error/ObjectNotFoundAdvice.java
+++ src/main/java/de/phib/tasket/data/shared/error/ObjectNotFoundAdvice.java
@@ -6,9 +6,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * ControllerAdvice to intercept ObjectNotFoundExceptions.
+ */
 @ControllerAdvice
 class ObjectNotFoundAdvice {
 
+    /**
+     * Intercepts an ObjectNotFoundException and returns status code 404 with the exception message as response body.
+     *
+     * @param e the exception
+     * @return the exception message as response body
+     */
     @ResponseBody
     @ExceptionHandler(ObjectNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
--- src/main/java/de/phib/tasket/data/shared/error/ObjectNotFoundException.java
+++ src/main/java/de/phib/tasket/data/shared/error/ObjectNotFoundException.java
@@ -1,7 +1,16 @@
 package de.phib.tasket.data.shared.error;
 
+/**
+ * Thrown, if no object can be found in a resository for a given id.
+ */
 public class ObjectNotFoundException extends RuntimeException {
 
+    /**
+     * Creates a new instance of ObjectNotFoundException.
+     *
+     * @param id         the id of the object that was not found
+     * @param objectType the type of the object that was not found
+     */
     public ObjectNotFoundException(String id, Class objectType) {
         super("Could not find object of type '" + objectType.getSimpleName() + "' with id '" + id + "'");
 
